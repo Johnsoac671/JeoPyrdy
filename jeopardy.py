9b89128919f1a443a9c8844a6dbe0a02893de222
Aidan Johnson <johnsoac671@gmail.com>
2023-07-05 14:01:47 -0700

    implemented a way to display the board

--- jeopardy.py
+++ jeopardy.py
@@ -1,17 +1,18 @@
 import json as j
 import random
-
-def generate_categories(num_of_cats=5):
-    with open("jeopardy_cats.txt", "r", encoding="utf-8") as file:
+import sys
+    
+def generate_categories(round, num=5):
+    with open(round+"jeopardy_cats.txt", "r", encoding="utf-8") as file:
         cats = file.readlines()
-        chosen = random.sample(cats, num_of_cats)
+        chosen = random.sample(cats, num)
         
         for x in range(len(chosen)):
             chosen[x] = chosen[x].replace("\n", "")
             
     return chosen
  
-def generate_questions(categories):   
+def generate_questions(categories, num=5):   
     with open("Questions.json", "r", encoding="utf-8") as json:
         all_questions = j.load(json)
         
@@ -19,12 +20,59 @@ def generate_questions(categories):
         
         for cat in categories:
             cat_questions = list(filter(lambda x: x["category"] == cat, all_questions))
-            cat_questions = random.sample(cat_questions, 5)
+            cat_questions = random.sample(cat_questions, num)
             
             selected_questions.append(sorted(cat_questions, key= lambda x: int(x["value"].replace("$", ""))))  
             
-        return selected_questions
+        return selected_questions 
+
+def start_game():
+    categories = generate_categories("")
+    board = [[200, 200, 200, 200, 200], [400, 400, 400, 400, 400], [600, 600, 600, 600, 600], [800, 800, 800, 800, 800], [1000, 1000, 1000, 1000, 1000]]
+    display_board(board, categories)
+    
+def display_board(current_board, categories):
+    cats = ""
+    for cat in categories:
+        cats += cat + " | "
         
-print(generate_questions(generate_categories()))         
+    print(cats)
+    for row in current_board:
+        board_row = ""
+        for col, box in enumerate(row):
+            
+            # Answered Questions
+            if box == "X":
+                
+                for x in range(len(categories[col])):
+                    board_row += "X"
+                    
+                board_row += " | "
+            
+            # Questions with values greater than 3 digits        
+            elif box > 999:
+                
+                for x in range(len(categories[col]) - 2):
+                    if x == (len(categories[col]) // 2) - 1:
+                        board_row += str(box)
+                        x += 40
+                    else:
+                        board_row += " "
+                board_row += "| "
+                
+            
+            # Questions with values equal to 3 digits    
+            else:
+                for x in range(len(categories[col]) - 1):
+                    
+                    if x == len(categories[col]) // 2:
+                        board_row += str(box)
+                    else:
+                        board_row += " "
+                        
+                board_row += "| "
+        print(board_row)
+    
+start_game()
         
     
