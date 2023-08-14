5aa0b5381097a7b1a77a0874a060af96614b7d6b
Aidan Johnson <johnsoac671@gmail.com>
2023-08-14 11:47:11 -0700

    removed chance for double jeopardy to only have one Daily Double

--- jeopardy.py
+++ jeopardy.py
@@ -8,9 +8,9 @@ import time
 # allow rewritting over lines instead of making new lines
 LINE_UP = '\033[1A'
 LINE_CLEAR = '\x1b[2K'
-MP3 = BytesIO()
+
 PLAYER_TOTAL = 0
-QUESTION_WORDS = ["what", "who", "when", "why", "how"]
+QUESTION_WORDS = ["what", "who", "where", "when", "why", "how"]
 ARTICLES = ["is", "are", "were", "was"]
 
 
@@ -21,7 +21,7 @@ class Question:
         self.answer = answer
         self.value = value
         self.category = category
-        self.daily_double = True
+        self.daily_double = False
 
     def __str__(self):
         return self.value
@@ -69,7 +69,7 @@ def generate_questions(categories, num=5):
         return selected_questions
 
 
-def get_selection(board):
+def get_selection(board, categories):
 
     while True:
         selection = input("Please select the next question: ").split(",")
@@ -87,75 +87,12 @@ def get_selection(board):
             continue
 
         else:
-            return selection
-
-
-def start_game():
-    global PLAYER_TOTAL
-    questions_answered = 0
-    round = 0
-
-    categories = generate_categories("")
-
-    board = [[200, 200, 200, 200, 200],
-             [400, 400, 400, 400, 400],
-             [600, 600, 600, 600, 600],
-             [800, 800, 800, 800, 800],
-             [1000, 1000, 1000, 1000, 1000]]
-
-    questions = generate_questions(categories)
-    for category in questions:
-        for index, question in enumerate(category):
-            question.value = (index + 1) * 200
-
-    while True:
-        if questions_answered == 1:
-            if round == 0:
-
-                print("Double Jeopardy!")
-                tts = gTTS(
-                    "Welcome to Double Jeopardy! In this round, question values are doubled.", lang="en", tld="co.uk")
-                tts.save("double_jep.mp3")
-                player = vlc.MediaPlayer("double_jep.mp3")
-                player.play()
-
+            if board[0] != "FINAL JEOPARDY!":
+                player = tts_speak(categories[int(selection[0]) - 1] + " for "
+                                   + str(board[int(selection[0]) - 1][int(selection[1]) - 1]) + ", please.", "selection", "com")
                 wait_for_tts(player)
-                clear_line(2)
 
-                board = [[400, 400, 400, 400, 400],
-                         [800, 800, 800, 800, 800],
-                         [1200, 1200, 1200, 1200, 1200],
-                         [1600, 1600, 1600, 1600, 1600],
-                         [2000, 2000, 2000, 2000, 2000]]
-
-                categories = generate_categories("double_")
-                questions = generate_questions(categories)
-
-                for category in questions:
-                    for index, question in enumerate(category):
-                        question.value = (index + 1) * 400
-
-            else:
-                board = ["FINAL JEOPARDY!"]
-
-                categories = generate_categories("final_", 1)
-                questions = generate_questions(categories, 1)
-
-            round += 1
-            questions_answered = 0
-
-        display_board(board, categories)
-
-        print("Player Total: " + str(PLAYER_TOTAL))
-
-        selection = get_selection(board)
-        selected_question = questions[int(
-            selection[0]) - 1][int(selection[1]) - 1]
-
-        display_question(selected_question, board[int(
-            selection[1]) - 1][int(selection[0]) - 1])
-        board[int(selection[1]) - 1][int(selection[0]) - 1] = "X"
-        questions_answered += 1
+            return selection
 
 
 def display_question(question, value):
@@ -163,55 +100,59 @@ def display_question(question, value):
     clear_line(8)
 
     if question.daily_double:
-        player = vlc.MediaPlayer("daily_double.mp3")
+        player = vlc.MediaPlayer("audio/daily_double.mp3")
         player.play()
 
         time.sleep(3)
 
-        tts = gTTS("   How much would you like to wager?",
-                   lang="en", tld="co.uk")
-        tts.save("wager.mp3")
-        player = vlc.MediaPlayer("wager.mp3")
-        player.play()
-
+        player = tts_speak(
+            "   You have found a Daily Double. How much would you like to wager?", "wager", "co.uk")
         wait_for_tts(player)
 
-        counter = 0
-        while True:
-            counter += 1
-            wager = input("Enter your wager: ")
-
-            if not wager.isdigit():
-                print("Error: Please enter an integer!")
-                continue
-            elif int(wager) > PLAYER_TOTAL:
-                if PLAYER_TOTAL > 1000:
-                    print(
-                        "Error: Please enter a wager less than your total: " + str(PLAYER_TOTAL))
-                    continue
-                elif int(wager) > 1000:
-                    print("Error: Please enter a wager less than or equal to 1000! ")
-                    continue
-            elif int(wager) < 0:
-                print("Error: Please enter a wager greater than or equal to 0! ")
-                continue
-
-            break
-
-        clear_line(counter)
-        question.value = int(wager)
+        question.value = get_wager()
+    
+    elif question.value == "null":
+        
+        question.value = get_wager()
 
     print(question.question)
 
-    tts = gTTS(question.question, lang="en", tld="co.uk")
-    tts.save("question.mp3")
-    player = vlc.MediaPlayer("question.mp3")
-    player.play()
-
+    player = tts_speak(question.question, "question", "co.uk")
     wait_for_tts(player)
 
     get_player_input(question)
     clear_line(3)
+
+
+def get_wager():
+
+    counter = 0
+    while True:
+        counter += 1
+        wager = input("Enter your wager: ")
+
+        if not wager.isdigit():
+            print("Error: Please enter an integer!")
+            continue
+        elif int(wager) > PLAYER_TOTAL:
+            if PLAYER_TOTAL > 1000:
+                print(
+                    "Error: Please enter a wager less than your total: " + str(PLAYER_TOTAL))
+                continue
+            elif int(wager) > 1000:
+                print("Error: Please enter a wager less than or equal to 1000! ")
+                continue
+        elif int(wager) < 0:
+            print("Error: Please enter a wager greater than or equal to 0! ")
+            continue
+
+        break
+
+    clear_line(counter)
+    player = tts_speak(str(wager), "wager_amount", "com")
+    wait_for_tts(player)
+
+    return int(wager)
 
 
 def get_player_input(question):
@@ -221,13 +162,9 @@ def get_player_input(question):
 
     player_answer = input("Your answer: ")
 
-    tts = gTTS(player_answer, lang="en", tld="com")
-    tts.save("player_answer.mp3")
-    player = vlc.MediaPlayer("player_answer.mp3")
-    player.play()
-
+    player = tts_speak(player_answer, "player_answer", "com")
     wait_for_tts(player)
-    
+
     # TODO: implement FINAL JEOPARDY! value system
     # checks if players answer is correct and also it is in the form of a question, kinda messy, probably better way to do this
     if (player_answer.lower().split()[0] in QUESTION_WORDS and player_answer.lower().split()[1] in ARTICLES
@@ -236,11 +173,7 @@ def get_player_input(question):
 
         print("That is correct!")
 
-        tts = gTTS("That is correct!", lang="en", tld="co.uk")
-        tts.save("correct.mp3")
-        player = vlc.MediaPlayer("correct.mp3")
-        player.play()
-
+        player = tts_speak("That is correct!", "correct", "co.uk")
         wait_for_tts(player)
 
         PLAYER_TOTAL += int(question.value)
@@ -248,21 +181,11 @@ def get_player_input(question):
     else:
         print("Sorry, that is incorrect. The correct answer was: " + question.answer)
 
-        tts = gTTS("Sorry, that is incorrect. The correct answer was:",
-                   lang="en", tld="co.uk")
-        tts.save("incorrect.mp3")
-        player = vlc.MediaPlayer("incorrect.mp3")
-        player.play()
-
+        player = tts_speak("Sorry, that is incorrect. The correct answer was: " +
+                           question.answer, "incorrect", "co.uk")
         wait_for_tts(player)
+
         PLAYER_TOTAL -= int(question.value)
-
-        tts = gTTS(question.answer, lang="en", tld="co.uk")
-        tts.save("answer.mp3")
-        player = vlc.MediaPlayer("answer.mp3")
-        player.play()
-
-        wait_for_tts(player)
 
 
 def display_board(current_board, categories):
@@ -311,6 +234,7 @@ def display_board(current_board, categories):
                 board_row += "| "
         print(board_row)
 
+
 # prints characters which clear previous lines
 def clear_line(num):
 
@@ -318,10 +242,113 @@ def clear_line(num):
         print(LINE_UP, end=LINE_CLEAR)
 
 # sleeps for length of tts message to avoid multiple messages speaking at once
+
+
 def wait_for_tts(player):
     time.sleep(0.5)
     duration = player.get_length() / 1000
     time.sleep(duration)
+
+
+def tts_speak(text, filename, voice):
+    tts = gTTS(text, lang="en", tld=voice)
+    tts.save("audio/" + filename + ".mp3")
+    player = vlc.MediaPlayer("audio/" + filename + ".mp3")
+    player.play()
+
+    return player
+
+
+def start_game():
+    global PLAYER_TOTAL
+    questions_answered = 0
+    round = 0
+
+    categories = generate_categories("")
+
+    board = [[200, 200, 200, 200, 200],
+             [400, 400, 400, 400, 400],
+             [600, 600, 600, 600, 600],
+             [800, 800, 800, 800, 800],
+             [1000, 1000, 1000, 1000, 1000]]
+
+    questions = generate_questions(categories)
+    for category in questions:
+        for index, question in enumerate(category):
+            question.value = (index + 1) * 200
+
+    dd_question = random.choice(random.choice(questions))
+    dd_question.daily_double = True
+
+    player = tts_speak("    The categories are: " +
+                       str(categories), "categories", "co.uk")
+    print(categories)
+    wait_for_tts(player)
+
+    clear_line(1)
+
+    while True:
+        if questions_answered == 25:
+            if round == 0:
+
+                print("Double Jeopardy!")
+                player = tts_speak(
+                    "    Welcome to Double Jeopardy! In this round, question values are doubled.", "double_jep", "co.uk")
+                wait_for_tts(player)
+
+                clear_line(2)
+
+                board = [[400, 400, 400, 400, 400],
+                         [800, 800, 800, 800, 800],
+                         [1200, 1200, 1200, 1200, 1200],
+                         [1600, 1600, 1600, 1600, 1600],
+                         [2000, 2000, 2000, 2000, 2000]]
+
+                categories = generate_categories("double_")
+                questions = generate_questions(categories)
+
+                for category in questions:
+                    for index, question in enumerate(category):
+                        question.value = (index + 1) * 400
+
+                dd_question = random.choice(random.choice(questions))
+                dd_question.daily_double = True
+
+                dd_question_2 = random.choice(random.choice(questions))
+                
+                while(dd_question_2.daily_double):
+                    dd_question_2 = random.choice(random.choice(questions))
+                    
+                dd_question.daily_double = True
+
+                player = tts_speak("    The categories are: " +
+                                   str(categories), "categories", "co.uk")
+                print(categories)
+                wait_for_tts(player)
+
+                clear_line(1)
+
+            else:
+                board = ["FINAL JEOPARDY!"]
+
+                categories = generate_categories("final_", 1)
+                questions = generate_questions(categories, 1)
+
+            round += 1
+            questions_answered = 0
+
+        display_board(board, categories)
+
+        print("Player Total: " + str(PLAYER_TOTAL))
+
+        selection = get_selection(board, categories)
+        selected_question = questions[int(
+            selection[0]) - 1][int(selection[1]) - 1]
+
+        display_question(selected_question, board[int(
+            selection[1]) - 1][int(selection[0]) - 1])
+        board[int(selection[1]) - 1][int(selection[0]) - 1] = "X"
+        questions_answered += 1
 
 
 start_game()
